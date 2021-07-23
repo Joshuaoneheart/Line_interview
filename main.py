3fd11c93aa27ff12cf8600f372badf707295df45
Joshuaoneheart <luna920804@DESKTOP-4083ONV.localdomain>
2021-07-23 13:24:59 +0800

    updated flow

--- main.py
+++ main.py
@@ -111,7 +111,7 @@ def handle_message(event):
         pass 
     elif message == "Does he take part in any projects?":
         pass
-    elif message == "Tell more about Joshua You.":
+    elif message == "Tell me more about Joshua You.":
         ret_message = TextSendMessage(
                 text='No problem! It is my pleasure to introduce my best friend. Which do you want to know?',
                 quick_reply=QuickReply(
